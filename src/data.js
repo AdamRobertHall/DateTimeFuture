cbb1bbe684c6cadcbb92976aec194760a23562e0
AdamLatop <adamroberthall@sina.com>
2017-07-29 19:53:42 +0800

    自定义ant

--- src/data.js
+++ src/data.js
@@ -2,17 +2,31 @@ import {calendar} from './base'
 
 let data = [];
 for(let year = 1900; year <= 2100; ++year) {
-    let oYear = {value: year, label: year + ''};
+    let oYear = {value: year, label: year + '年'};
     oYear.children = [];
     for (let month = 1; month <= 12; ++ month) {
-        let oMonth = {value: month - 1, label: month + ''};
+        let oMonth = {value: month, label: month + '月'};
         oMonth.children = [];
-        let days = calendar.solarDays(year, month - 1);
+        let days = calendar.solarDays(year, month);
         for (let day = 1; day <= days; ++day) {
-            let oDay = {value: day, label: day + ''};
+            let oDay = {value: day, label: day + '日'};
             oDay.children = [];
             for (let hour = 0; hour < 24; ++ hour) {
-                oDay.children.push({value: hour, label: hour + ''});
+                let label = hour;
+                if (hour < 10) {
+                    label = '0' + hour;
+                }
+                let oHour = {value: hour, label: label + ''};
+                oHour.children = [];
+                for (let min = 0; min < 60; min += 5) {
+                    let label = min;
+                    if (min < 10) {
+                        label = '0' + min;
+                    }
+                    let oMin = {value: min, label: label + ''};
+                    oHour.children.push(oMin);
+                }
+                oDay.children.push(oHour);
             }
             oMonth.children.push(oDay);
         }
@@ -22,31 +36,31 @@ for(let year = 1900; year <= 2100; ++year) {
 }
 let ldata = [];
 for(let year = 1900; year <= 2100; ++year) {
-    let oYear = {value: year, label: year + ''};
+    let oYear = {value: year, label: year};
     oYear.children = [];
     let leapMonth = calendar.leapMonth(year);
-    for (let month = 0; month < 12; ++ month) {
-        let oMonth = {value: month, label: calendar.toChinaMonth(month + 1)};
+    for (let month = 1; month <= 12; ++ month) {
+        let oMonth = {value: month, label: calendar.toChinaMonth(month)};
         oMonth.children = [];
-        let days = calendar.solarDays(year, month - 1);
+        let days = calendar.monthDays(year, month);
         for (let day = 1; day <= days; ++day) {
             let oDay = {value: day, label: calendar.toChinaDay(day)};
             oDay.children = [];
             for (let hour = 0; hour < 12; ++ hour) {
-                oDay.children.push({value: hour, label: calendar.Zhi[hour]});
+                oDay.children.push({value: hour, label: calendar.Zhi[hour] + '时'});
             }
             oMonth.children.push(oDay);
         }
         oYear.children.push(oMonth);
-        if (leapMonth > 0 && month + 1 === leapMonth) {
-            let oMonth = {value: month, label: '\u95f0' + calendar.toChinaMonth(month + 1)};
+        if (leapMonth > 0 && month === leapMonth) {
+            let oMonth = {value: month + 12, label: '\u95f0' + calendar.toChinaMonth(month)};
             oMonth.children = [];
             let leapDays = calendar.leapDays(year);
-            for (let day = 1; day <= days; ++day) {
+            for (let day = 1; day <= leapDays; ++day) {
                 let oDay = {value: day, label: calendar.toChinaDay(day)};
                 oDay.children = [];
                 for (let hour = 0; hour < 12; ++ hour) {
-                    oDay.children.push({value: hour, label: calendar.Zhi[hour]});
+                    oDay.children.push({value: hour, label: calendar.Zhi[hour] + '时'});
                 }
                 oMonth.children.push(oDay);
             }
@@ -54,7 +68,7 @@ for(let year = 1900; year <= 2100; ++year) {
         }
     }
     
-    data.push(oYear);
+    ldata.push(oYear);
 }
 export const pickerData = data;
 export const pickerLdata = ldata;
--- src/index.js
+++ src/index.js
@@ -5,33 +5,87 @@ import {
 	Text,
 	View
 } from 'react-native';
-import {Picker} from 'antd-mobile';
+import Picker from './com/picker';
+import List from './com/list';
+import InputItem from './com/input-item'
 import {pickerData, pickerLdata} from './data';
+import {calendar} from './base'
 export default class DateTimeFuture extends Component {
+	constructor() {
+		super();
+		this.state = {
+			value: [2000, 1, 1, 0, 0],
+			lvalue: [2000, 1, 1, 0],
+			bzValue: null
+		}
+	}
     onSelectDateTime(value) {
-        console.log(value, 'value')
+        this.setState({value: value});
     }
 	onSelectLdateTime(value) {
-		console.log(value, 'value')
+		this.setState({lvalue: value});
+	}
+	dateFormat(values) {
+		//console.log(values, 'values');
+		let str = "";
+		for (let i in values) {
+			str += values[i].label;
+			if (i == 2) {
+				str += "  ";
+			}
+			if (i == 3) {
+				str += ":";
+			}
+		}
+		//console.log(str, 'str');
+		return str;
+	}
+	ldateFormat(values) {
+		console.log(values, 'values');
+		let obj = calendar.lunar2solar(
+			values[0].value,
+			values[1].value > 12 ? values[1].value - 12 : values[1].value,
+			values[2].value,
+			values[1].value > 12,
+			values[3].value,
+		);
+		let str = "";
+		for (let i in values) {
+			str += values[i].label;
+			if (i == 0) {
+				str += obj.Animal + '年';
+			} 
+		}
+		str += '(' + obj.gzYear + obj.gzMonth + obj.gzDay + obj.gzH + ')';
+		//this.setState({bzValue: bzstr});
+		return str;
 	}
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text style={styles.welcome}>
-					Welcome to DateTimeFuture
-				</Text>
-				<Picker
-                    title='公历'
-                    data={pickerData}
-                    cols={4}
-                    onOk={this.onSelectDateTime(value).bind(this)}
-				/>
-				<Picker
-                    title='公历'
-                    data={pickerLdata}
-                    cols={4}
-                    onOk={this.onSelectLdateTime(value).bind(this)}
-				/>
+				<List>
+					<Picker
+						title='公历'
+						data={pickerData}
+						cols={5}
+						value={this.state.value}
+						onOk={this.onSelectDateTime.bind(this)}
+						format={this.dateFormat.bind(this)}
+					>
+						<List.Item multipleLine arrow="horizontal">选择公历</List.Item>
+					</Picker>
+					<Picker
+						title='农历'
+						data={pickerLdata}
+						cols={4}
+						value={this.state.lvalue}
+						onOk={this.onSelectLdateTime.bind(this)}
+						format={this.ldateFormat.bind(this)}
+					>
+						<List.Item multipleLine arrow="horizontal">农历</List.Item>
+					</Picker>
+				</List>
+				
 			</View>
 		);
 	}
@@ -40,9 +94,10 @@ export default class DateTimeFuture extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
+	},
+	flexRow: {
+		flex: 1,
+		flexDirection: 'row'
 	},
 	welcome: {
 		fontSize: 20,
